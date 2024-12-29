1dc02beaeb263ccf7c361738ab6962653f9d1304
Azriel Hoh <azriel91@gmail.com>
2024-12-30 10:48:36 +1300

    Remove unused types.

--- crate/stacktrace/src/lib.rs
+++ crate/stacktrace/src/lib.rs
@@ -1,14 +1,6 @@
 //! Data types representing a stack trace.
 
-pub use crate::{
-    group::Group, group_or_segment::GroupOrSegment, line::Line, render_mode::RenderMode,
-    section::Section, segment::Segment, stacktrace::Stacktrace,
-};
+pub use crate::{section::Section, stacktrace::Stacktrace};
 
-mod group;
-mod group_or_segment;
-mod line;
-mod render_mode;
 mod section;
-mod segment;
 mod stacktrace;
--- crate/stacktrace/src/section.rs
+++ crate/stacktrace/src/section.rs
@@ -1,5 +1,3 @@
-use crate::Group;
-
 /// A hierarchical structure of frames that have the same leading [`Group`]s.
 ///
 /// Constructed from a group of [`Line`]s.
@@ -7,12 +5,10 @@ use crate::Group;
 /// [`Line`]: crate::Line
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Section {
-    /// Any leading whitespace for this section.
-    ///
-    /// This does not include leading whitespace from parent `Section`s.
-    pub leading_whitespace: String,
-    /// The groups of segments common to this section and child sections.
-    pub groups: Vec<Group>,
+    /// Slice of the line that is common with ancestor `Section`s.
+    pub slice_common_with_ancestors: String,
+    /// Slice of the line that is not common with ancestors.
+    pub slice_remainder: String,
     /// Child `Section`s of this section.
     pub child_sections: Vec<Section>,
 }
