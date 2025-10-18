a05c09b0c2729a3972e96fb26f7b30d1265ee9af
Azriel Hoh <azriel91@gmail.com>
2025-10-18 19:17:30 +1300

    Add custom buttons for `expand_level` input.

--- playground/src/components/sem_log_viewer_controls_div.rs
+++ playground/src/components/sem_log_viewer_controls_div.rs
@@ -1,24 +1,47 @@
 use leptos::{
     component,
     ev::InputEvent,
-    prelude::{ClassAttribute, ElementChild, Get, OnAttribute, ReadSignal, Set, WriteSignal},
+    prelude::{
+        ClassAttribute, ElementChild, Get, OnAttribute, PropAttribute, ReadSignal, Set, WriteSignal,
+    },
     view, IntoView,
 };
 use wasm_bindgen::JsCast;
 
 const SEM_LOG_VIEWER_CONTROLS_DIV_CLASSES: &str = "\
-    bg-slate-700 \
-    text-slate-100 \
-    font-mono \
-    \
     w-full \
     lg:max-w-7xl \
-    p-4 \
+    p-2 \
     rounded-lg \
-    shadow-[inset_0_2px_4px_0_rgba(0,0,0,0.3)] \
     \
-    overflow-scroll \
-    text-nowrap \
+    flex \
+";
+
+const EXPAND_LEVEL_INPUT_CLASSES: &str = "\
+    bg-blue-900 \
+    px-2 \
+    w-10 \
+    text-right \
+    rounded \
+    border \
+    border-blue-950 \
+";
+
+/// Tailwind classes for the `expand_level` "+" and "-" buttons.
+const EXPAND_LEVEL_BUTTON_CLASSES: &str = "\
+    bg-blue-500 \
+    border-blue-600 \
+    hover:bg-blue-600 \
+    hover:border-blue-800 \
+    active:bg-blue-700 \
+    active:border-blue-900 \
+    text-white \
+    font-bold \
+    inline-block \
+    w-6 \
+    h-6 \
+    rounded \
+    border \
 ";
 
 #[component]
@@ -28,12 +51,15 @@ pub fn SemLogViewerControlsDiv(
 ) -> impl IntoView {
     view! {
         <div class=SEM_LOG_VIEWER_CONTROLS_DIV_CLASSES>
-            <label>"Expand level:"</label>
+            <label for="expand_level">"Expand level:"</label>
+            <div class="w-2"></div>
             <input
+                name="expand_level"
                 type="number"
                 min="0"
                 max="10"
-                value=move || block_expand_level.get()
+                class=EXPAND_LEVEL_INPUT_CLASSES
+                prop:value=move || block_expand_level.get()
                 on:input=move |ev| {
                     let value = ev.unchecked_ref::<InputEvent>()
                         .data()
@@ -43,6 +69,45 @@ pub fn SemLogViewerControlsDiv(
                     }
                 }
             />
+            <div class="w-2"></div>
+            <ExpandLevelSubtractButton block_expand_level block_expand_level_set />
+            <ExpandLevelAddButton block_expand_level block_expand_level_set />
         </div>
+    }
+}
+
+#[component]
+fn ExpandLevelSubtractButton(
+    block_expand_level: ReadSignal<u8>,
+    block_expand_level_set: WriteSignal<u8>,
+) -> impl IntoView {
+    view! {
+        <button
+            class=EXPAND_LEVEL_BUTTON_CLASSES
+            on:click=move |_event| {
+                let next_value = block_expand_level.get().saturating_sub(1);
+                block_expand_level_set.set(next_value);
+            }
+        >
+            "-"
+        </button>
+    }
+}
+
+#[component]
+fn ExpandLevelAddButton(
+    block_expand_level: ReadSignal<u8>,
+    block_expand_level_set: WriteSignal<u8>,
+) -> impl IntoView {
+    view! {
+        <button
+            class=EXPAND_LEVEL_BUTTON_CLASSES
+            on:click=move |_event| {
+                let next_value = block_expand_level.get().saturating_add(1);
+                block_expand_level_set.set(next_value);
+            }
+        >
+            "+"
+        </button>
     }
 }
